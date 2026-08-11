c5858b7cf0b7dc77bd1cff99ad249549507a2d40
elhajzoubir <aymanminou1998@gmail.com>
2026-08-11 17:13:51 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,3 @@
-const CACHE_NAME = "alhaj-zoubir-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      cached => cached || fetch(event.request)
-    )
-  );
-});
+const CACHE='hz-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
