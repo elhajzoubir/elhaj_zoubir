b51d37414e91758eef1f5541992648ba4e1b65bb
elhajzoubir <aymanminou1998@gmail.com>
2026-08-26 16:27:35 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// إعدادات Firebase Web App الخاصة بالحاج الزوبير
-window.HZ_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBOThAl5z7W6I3h3-Qss7m9LMsGrrEA3Kw",
-  authDomain: "elhaj-zoubir1.firebaseapp.com",
-  projectId: "elhaj-zoubir1",
-  storageBucket: "elhaj-zoubir1.firebasestorage.app",
-  messagingSenderId: "336297199471",
-  appId: "1:336297199471:web:41478e94eb9c6c28818a9a"
-};
