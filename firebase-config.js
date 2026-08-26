3bfd64e199aa3b1bac2e7c3cde32b5c73f6b8eb4
elhajzoubir <aymanminou1998@gmail.com>
2026-08-26 18:14:58 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase configuration for Al-Hajj Zoubir
-export const firebaseConfig = {
-  apiKey: "AIzaSyBOThAl5z7W6I3h3-Qss7m9LMsGrrEA3Kw",
-  authDomain: "elhaj-zoubir1.firebaseapp.com",
-  projectId: "elhaj-zoubir1",
-  storageBucket: "elhaj-zoubir1.firebasestorage.app",
-  messagingSenderId: "336297199471",
-  appId: "1:336297199471:web:41478e94eb9c6c28818a9a"
-};
